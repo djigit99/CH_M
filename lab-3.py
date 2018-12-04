fa4bb208b643ee26d4e25f4247abc257362042b1
Andrii Perun <andrewfreelan@gmail.com>
2018-12-05 00:34:32 +0200

    Two methods done. One more need

---
+++ lab-3.py
@@ -0,0 +1,119 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def pol_lagr_form(n, x, y, x0):
+    L = 0
+    for i in range(n):
+        d1 = 1
+        d2 = 1
+        for j in range(n):
+            if (j == i):
+                continue
+            d1 *= x0 -  x[j]
+            d2 *= x[i] - x[j]
+        L += y[i] * d1 / d2
+    return L
+
+def draw_lagr(n, x, y, a, b, eps):
+    x_p = []
+    y_p = []
+    ind = 0
+    while a <= b:
+        x_p.append(a)
+        y_p.append(pol_lagr_form(n, x, y, x_p[ind]))
+        ind += 1 
+        a += eps 
+
+    plt.plot(x_p, y_p)
+    plt.grid(True)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+    return 
+
+def progonka_method(n, a, b):
+    A, B = np.zeros(n), np.zeros(n)
+    for i in range(n):
+        if i == 0:
+            y = a[i][i]
+            A[i] = - (a[i][i+1] / y)
+            B[i] = b[i] / y
+        elif i == n-1:
+            y = a[i][i] + a[i][i-1] * A[i-1]
+            B[i] = (b[i] - a[i][i-1] * B[i-1]) / y
+        else:
+            y = a[i][i] + a[i][i-1] * A[i-1]
+            A[i] = - (a[i][i+1] / y)
+            B[i] = (b[i] - a[i][i-1] * B[i-1]) / y
+    x = np.zeros(n)
+    x[n-1] = B[n-1]
+    for i in range(n-2, -1, -1):
+        x[i] = A[i] * x[i+1] + B[i]
+    return x
+
+def cub_spline(n, x, y, eps):
+    a = []
+    b = []
+    d = []
+    h = []
+    A = np.zeros((n,n))
+    B = np.zeros(n)
+    h.append(0)
+    for i in range(1, n+1):
+        a.append(y[i-1])
+        h.append(x[i] - x[i-1])
+    A[0][0] = 1
+    A[n-1][n-1] = 1
+    for i in range(1, n-1):
+        A[i][i] = 2 * (h[i] + h[i+1])
+        A[i][i-1] = h[i]
+        A[i][i+1] = h[i+1]
+        B[i] = 3 * ((y[i+1] - y[i]) / h[i+1] - (y[i] - y[i-1]) / h[i])
+    c = progonka_method(n, A, B)
+    print(c)
+
+    for i in range(n-1):
+        d.append( (c[i+1] - c[i]) / (3 * h[i+1]) )
+        b.append( ((y[i+1] - y[i]) / h[i+1]) - (h[i+1] / 3) * (c[i+1] + 2 * c[i]) )
+    d.append( -c[n-1] / (3 * h[n]) )
+    b.append( ((y[n] - y[n-1]) / h[n]) - (2 * h[n] * c[n-1]) / 3 )
+    print(d)
+
+    x_p = []
+    y_p = []
+    ind = 0
+    l = x[0]
+    r = x[n]
+    while l <= r:
+        if (l>=x[ind+1]):
+            ind += 1
+        xx = l - x[ind]
+        print(xx, a[ind], b[ind], c[ind], d[ind])
+        x_p.append(l)
+        y_p.append(a[ind] + b[ind] * xx + c[ind] * xx * xx + d[ind] * xx * xx * xx )
+        l += eps 
+
+    plt.plot(x_p, y_p)
+    plt.grid(True)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+    return 
+
+#tests
+
+#cub_splain
+# n = 6
+# x = -1.5 -1 -0.5 0 0.5 1 1.5
+# y = -0.7 0 0.7 1 0.7 0 -0.7
+def main():
+    eps = 0.2
+    n = int(input("Enter n : "))
+    #a = int(input("Enter a : "))
+    #b = int(input("Enter b : "))
+    x = [float(i) for i in input("Enter x_i: ").split()]
+    y = [float(i) for i in input("Enter y_i: ").split()]
+    cub_spline(n, x, y, eps)
+
+if __name__ == "__main__":
+    main()
