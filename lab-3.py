d0d7bff6aeb46fa919cb8716cc94d5f4f9e37f47
Andrii Perun <andrewfreelan@gmail.com>
2018-12-05 02:11:41 +0200

    Newton methon is added

--- lab-3.py
+++ lab-3.py
@@ -70,14 +70,12 @@ def cub_spline(n, x, y, eps):
         A[i][i+1] = h[i+1]
         B[i] = 3 * ((y[i+1] - y[i]) / h[i+1] - (y[i] - y[i-1]) / h[i])
     c = progonka_method(n, A, B)
-    print(c)
 
     for i in range(n-1):
         d.append( (c[i+1] - c[i]) / (3 * h[i+1]) )
         b.append( ((y[i+1] - y[i]) / h[i+1]) - (h[i+1] / 3) * (c[i+1] + 2 * c[i]) )
     d.append( -c[n-1] / (3 * h[n]) )
     b.append( ((y[n] - y[n-1]) / h[n]) - (2 * h[n] * c[n-1]) / 3 )
-    print(d)
 
     x_p = []
     y_p = []
@@ -88,7 +86,6 @@ def cub_spline(n, x, y, eps):
         if (l>=x[ind+1]):
             ind += 1
         xx = l - x[ind]
-        print(xx, a[ind], b[ind], c[ind], d[ind])
         x_p.append(l)
         y_p.append(a[ind] + b[ind] * xx + c[ind] * xx * xx + d[ind] * xx * xx * xx )
         l += eps 
@@ -100,17 +97,59 @@ def cub_spline(n, x, y, eps):
     plt.show()
     return 
 
+def newton_form(n, x, y, eps):
+    a = []
+    delta = y
+    h = x[1] - x[0]
+    fact = []
+    fact.append(1)
+    for i in range(1, n):
+        fact.append(fact[i-1] * (i+1) )
+    a.append(y[0])
+    for i in range(n):
+        for j in range(n-i):
+            delta[j] = delta[j+1] - delta[j]
+        a.append(delta[0] /  fact[i])
+
+    x_p = []
+    y_p = []
+    l = x[0]
+    r = x[n]
+    while l <= r:
+        x_p.append(l)
+        S = 0
+        q = 1
+        for i in range(n+1):
+            S += a[i] * q
+            q *= (l - x[0]) / h - i
+        y_p.append(S)
+        l += eps 
+    
+    plt.plot(x_p, y_p)
+    plt.grid(True)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+    return 
 #tests
 
-#cub_splain
+#cub_splain and newton
 # n = 6
 # x = -1.5 -1 -0.5 0 0.5 1 1.5
 # y = -0.7 0 0.7 1 0.7 0 -0.7
+
+#lagr
+# n = 7
+# a = -1.5
+# b = 1.5
+# x = -1.5 -1 -0.5 0 0.5 1 1.5
+# y = -0.7 0 0.7 1 0.7 0 -0.7
+
 def main():
     eps = 0.2
     n = int(input("Enter n : "))
-    #a = int(input("Enter a : "))
-    #b = int(input("Enter b : "))
+    #a = float(input("Enter a : "))
+    #b = float(input("Enter b : "))
     x = [float(i) for i in input("Enter x_i: ").split()]
     y = [float(i) for i in input("Enter y_i: ").split()]
     cub_spline(n, x, y, eps)
